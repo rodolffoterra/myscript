cdbbe73a2767edfe6b1aa506a63d14f82d18d9e4
Rodolfo Terra <55515296+rodolffoterra@users.noreply.github.com>
2020-10-22 23:04:50 -0300

    Update geobr_plot_cidade_sp.R

--- geobr_plot_cidade_sp.R
+++ geobr_plot_cidade_sp.R
@@ -3,14 +3,14 @@ install.packages("geobr")
 library(geobr)
 
 sp <- geobr::read_census_tract(
-  code_tract = 3550308,
-  year = 2010,
+  code_tract = 3550308, # código IBGE
+  year = 2010, # ano
   simplified = TRUE,
   showProgress = TRUE
 )
 
 
-# plotar
+# plot
 
 no_axis <- theme(axis.title=element_blank(),
                  axis.text=element_blank(),
@@ -18,7 +18,7 @@ no_axis <- theme(axis.title=element_blank(),
 
 
 
-# Plot all Brazilian states
+# Plot all São Paulo city
 ggplot() +
   geom_sf(data=sp, fill="#2D3E50", color="#FEBF57", size=.15, show.legend = FALSE) +
   labs(subtitle="States", size=8) +
