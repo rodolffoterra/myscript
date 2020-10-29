6c7ba3dcdea93603283bf2fa62b5f895c283cb85
Rodolfo Terra <55515296+rodolffoterra@users.noreply.github.com>
2020-10-28 22:15:54 -0300

    Update and rename merge to merge.r

--- merge.r
+++ merge.r
@@ -1,3 +1,17 @@
+
+By using the merge function and its optional parameters:
+
+Inner join: merge(df1, df2) will work for these examples because R automatically joins the frames by common variable names, but you would most likely want to specify merge(df1, df2, by = "CustomerId") to make sure that you were matching on only the fields you desired. You can also use the by.x and by.y parameters if the matching variables have different names in the different data frames.
+
+Outer join: merge(x = df1, y = df2, by = "CustomerId", all = TRUE)
+
+Left outer: merge(x = df1, y = df2, by = "CustomerId", all.x = TRUE)
+
+Right outer: merge(x = df1, y = df2, by = "CustomerId", all.y = TRUE)
+
+Cross join: merge(x = df1, y = df2, by = NULL)
+
+
 uthors <- data.frame(
     ## I(*) : use character columns of names to get sensible sort order
     surname = I(c("Tukey", "Venables", "Tierney", "Ripley", "McNeil")),
